1db91d621894fdc1c27dba8be4df2bf6e3a72cfb
Kristofer <kristofer@fmckl.se>
2024-10-17 10:26:51 +0200

    Cache eth address in local storage

--- src/hooks/useEthAddress.tsx
+++ src/hooks/useEthAddress.tsx
@@ -1,30 +1,55 @@
 import { useActor } from "@/actor";
-import { Principal } from "@dfinity/principal";
 import { useQuery } from "@tanstack/react-query";
 import useHandleAgentError from "./useHandleAgentError";
+import { useInternetIdentity } from "ic-use-internet-identity";
 
-export default function useEthAddress(principal?: Principal) {
+export default function useEthAddress() {
   const { actor: basic_eth } = useActor();
   const { handleAgentError } = useHandleAgentError();
+  const { identity } = useInternetIdentity();
+  const principal = identity?.getPrincipal();
+
+  // This query caches the generated Ethereum address in local storage
+  // to minimize the number of calls to the backend and to improve UX.
   return useQuery({
     queryKey: ['address', principal],
     queryFn: async () => {
+
+      if (!principal) {
+        throw new Error("Principal is required.");
+      }
+
+      const cacheKey = `ethAddress-${principal.toText()}`;
+      const cachedAddress = localStorage.getItem(cacheKey);
+
+      if (cachedAddress) {
+        // Return cached address if available
+        return cachedAddress;
+      }
+
       try {
-        const result = await basic_eth?.get_address(principal ? [principal] : []);
+        const result = await basic_eth?.get_address([principal]);
+
         if (result === undefined) {
-          throw new Error("Undefined address returned.")
+          throw new Error("Undefined address returned.");
         }
+
         if ('Err' in result) {
           throw new Error(result.Err);
         }
-        return result.Ok;
+
+        const address = result.Ok;
+
+        // Cache the fetched address
+        localStorage.setItem(cacheKey, address);
+
+        return address;
       } catch (e) {
         handleAgentError(e);
-        console.error(e)
-        throw new Error("Invalid address returned.")
+        console.error(e);
+        throw new Error("Invalid address returned.");
       }
     },
-    enabled: !!basic_eth
-  })
-
+    enabled: !!basic_eth && !!principal,
+  });
 }
