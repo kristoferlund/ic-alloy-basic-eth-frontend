c3cf818ac2269595d39f60219e0eb445ece146c5
Kristofer <kristofer@fmckl.se>
2024-10-17 14:47:35 +0200

    License etc

--- src/App.tsx
+++ src/App.tsx
@@ -19,6 +19,25 @@ export default function App() {
   return <main>
     <AppInner />
     <Toaster />
+
+    <div className="links">
+      <a
+        href="https://github.com/kristoferlund/ic-alloy-basic-eth-frontend"
+        target="_blank" rel="noreferrer"
+      >
+        <img src="https://img.shields.io/github/license/kristoferlund/ic-alloy-basic-eth-frontend" />
+      </a>
+
+      <a
+        href="https://github.com/kristoferlund/ic-alloy-basic-eth-frontend"
+        target="_blank" rel="noreferrer"
+      >
+        <img src="https://img.shields.io/github/stars/kristoferlund/ic-alloy-basic-eth-frontend" />
+      </a>
+      <a href="https://github.com/kristoferlund" target="_blank" rel="noreferrer">
+        <img src="https://img.shields.io/github/followers/kristoferlund" />
+      </a>
+    </div>
   </main>
 }
 
